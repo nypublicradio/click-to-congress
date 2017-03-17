02dee1b15570bf6ff550b7a3fbb39883e257b666
Matt Oberle <moberle@nypublicradio.org>
2017-03-17 16:50:55 -0400

    moves healthcheck to the root of the app
    The AWS ELB checks for a 204 response at /.
    The healthcehck endpoint needs to be independent of the apiRouter
    prefix.

--- index.js
+++ index.js
@@ -12,10 +12,10 @@ morgan.token('remote-addr', function (req, res) {
   return realIP || req.connection.remoteAddress;
 });
 
-apiRouter.use('/', require('./routes/healthcheck'));
 apiRouter.use('/v1/lookup', require('./routes/lookup'));
 apiRouter.use('/v1/call', require('./routes/call'));
 apiRouter.use('/v1/connect', require('./routes/connect'));
+app.use('/', require('./routes/healthcheck'));
 app.use(morgan('combined'));
 app.use(apiPrefix, apiRouter);
 
