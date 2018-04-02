854271316dfc2043618e4aff07affcb5d939b705
Brian Whitton <me@brianwhitton.com>
2018-04-02 09:26:02 -0400

    use cors in development

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 var express = require('express');
 var morgan = require('morgan');
+var cors = require('cors');
 var apiRouter = express.Router();
 var app = express();
 
@@ -11,6 +12,10 @@ morgan.token('remote-addr', function (req, res) {
   var realIP = req.headers['x-real-ip'];
   return realIP || req.connection.remoteAddress;
 });
+
+if (process.env.NODE_DEV) {
+  app.use(cors());
+}
 
 apiRouter.use('/v1/lookup', require('./routes/lookup'));
 apiRouter.use('/v1/call', require('./routes/call'));
